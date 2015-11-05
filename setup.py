fab48c45b5be982af33ffe42f12ae321e1c2d815
Josu B <josubg@gmail.com>
2015-11-05 19:55:58 +0100

    Version number updated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='pynaf',
-    version='1.0',
+    version='2.0',
     author='Rodrigo Agerri',
     author_email='rodrigo.agerri@ehu.es',
     packages=['pynaf',],
