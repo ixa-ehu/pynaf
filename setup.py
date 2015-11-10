f51796f3db3557945d111b8c87997d9ddc891641
josu <josubg@gmail.com>
2015-11-10 13:47:11 +0100

    Kaf and Naf integration in one library.

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
+# coding=utf-8
+""" Pykaf setup.py package.
+
+    https://docs.python.org/2/distutils/index.html
+
+"""
 from distutils.core import setup
 
 setup(
     name='pynaf',
-    version='1.0',
+    version='2.0',
     author='Rodrigo Agerri',
     author_email='rodrigo.agerri@ehu.es',
     packages=['pynaf',],
-    url='https://github.com/ixa-ehu/pynaf',
+    url='https://github.com/josubg/pynaf/',
     license='LICENSE',
     description='Read and create NAF annotation Documents.',
-    install_requires=[],
+    install_requires=['lxml'],
     long_description=open('README.md').read(),
 )
 
