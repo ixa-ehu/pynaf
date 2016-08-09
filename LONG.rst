01c0bd46c6b8b0b68eadeaa13aabd3d97205ba03
josubg <josubg@gmail.com>
2016-08-09 16:51:55 +0200

    Added to pypi

---
+++ LONG.rst
@@ -0,0 +1,8 @@
+pynaf
+=====
+
+**pynaf** provides  a simple API for creating or reading **NAF documents**.
+
+The NAF (Natural language Annotation Format) is an XML markup language that contains several layers of linguistic annotation. It is used in **Natural Language Processing** tools like `IXA Pipes <http://ixa2.si.ehu.es/ixa-pipes/>`_ or `Corefgraph <https://bitbucket.org/Josu/corefgraph>`_.
+
+More information about NAF in the  following `link <https://github.com/newsreader/NAF>`_.
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
     https://docs.python.org/2/distutils/index.html
 
 """
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='pynaf',
@@ -13,9 +13,19 @@ setup(
     author_email='rodrigo.agerri@ehu.es',
     packages=['pynaf',],
     url='https://github.com/josubg/pynaf/',
-    license='LICENSE',
+    license=' Apache License 2.0 (APL 2.0)',
     description='Read and create NAF annotation Documents.',
-    install_requires=['lxml'],
-    long_description=open('README.md').read(),
+    long_description=open('LONG.rst').read(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Processing :: Markup :: XML', 
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords='NLP XML markup NAF',
+    install_requires=['lxml']
 )
 
